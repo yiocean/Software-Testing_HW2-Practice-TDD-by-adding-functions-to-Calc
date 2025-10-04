bafbb3fef4c47325e76d1a4daaa3a4d02b22dc2b
yiocean <amber.555yi@gmail.com>
2025-10-04 16:33:07 +0800

    init commit

---
+++ CalcTest.py
@@ -0,0 +1,11 @@
+import unittest
+from calculator import Calculator  # The class we are going to implement
+
+class TestCalculator(unittest.TestCase):
+    def test_add(self):
+        calc = Calculator()
+        result = calc.add(2, 3)
+        self.assertEqual(result, 5)  # Expect 2 + 3 = 5
+
+if __name__ == "__main__":
+    unittest.main()
